de8890d149e4b43cdee7183a484ec416a109e80e
midcondria <hyukkind@naver.com>
2024-04-01 23:41:54 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/example/nikkebe/NikkeBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.nikkebe;
+
+import com.opencsv.CSVReader;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikkeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikkeBeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/nikkebe/NikkeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nikkebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikkeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
